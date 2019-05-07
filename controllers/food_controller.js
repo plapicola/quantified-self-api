456bbfd2cb3288f89b1dfb82dd3db4d1dd2f1a57
plapicola <p.lapicola@gmail.com>
2019-05-07 14:32:39 -0600

    Updates test to force exit

--- controllers/food_controller.js
+++ controllers/food_controller.js
@@ -3,6 +3,7 @@ var FoodSerializer = require('../serializers/food_serializer');
 
 module.exports = class FoodController {
   static index(request, response) {
+    response.setHeader("Content-Type", "application/json");
     Food.findAll()
       .then(foods => {
         response.status(200).send(FoodSerializer.formatAll(foods))
