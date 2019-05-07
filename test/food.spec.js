4fe81d3348d5131e2da95d61226b04a8b400bcaf
plapicola <p.lapicola@gmail.com>
2019-05-07 13:13:11 -0600

    Adds tests for foods get endpoint

---
+++ test/food.spec.js
@@ -0,0 +1,24 @@
+var shell = require('shelljs');
+var request = require('supertest');
+var app = require('../app');
+
+describe('Food api', () => {
+  beforeEach(() => {
+    shell.exec('npx sequelize db:migrate:undo:all');
+    shell.exec('npx sequelize db:create');
+    shell.exec('npx sequelize db:migrate');
+    shell.exec('npx sequelize db:seed:all');
+  })
+
+  describe('Get games path', () => {
+    test('Should return all food items in the system', () => {
+      return request(app).get('/api/v1/foods')
+        .then(response => {
+          expect(response.status).toBe(200)
+          expect(response.body.length).toBe(3)
+          expect(response.body[0].name).toBe("Banana")
+          expect(response.body[0].calories).toBe(150)
+        })
+    })
+  })
+})
