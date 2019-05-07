3225a14a459a8016f0ecfa096d1e6ebdd8892ba5
plapicola <p.lapicola@gmail.com>
2019-05-07 13:31:25 -0600

    Updates food controller to implement a serializer

--- controllers/food_controller.js
+++ controllers/food_controller.js
@@ -1,10 +1,11 @@
 var Food = require('../models').Food;
+var FoodSerializer = require('../serializers/food');
 
 module.exports = class FoodController {
   static index(request, response) {
     Food.findAll()
       .then(foods => {
-        response.status(200).send(foods)
+        response.status(200).send(FoodSerializer.formatAll(foods))
       })
   }
 }
