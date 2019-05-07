d0a5a9feaf65d2309b2cfc8692afabcf5f651e17
Ty Mazey <tymazey@gmail.com>
2019-05-07 16:29:09 -0600

    adds path for deleting food item to router

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -6,6 +6,8 @@ var FoodController = require('../../../controllers/food_controller')
 router.get('/', FoodController.index);
 /* GET single food item */
 router.get('/:id', FoodController.show);
+/* DELETE single food item */
+router.get('/:id', FoodController.remove);
 
 
 module.exports = router;
