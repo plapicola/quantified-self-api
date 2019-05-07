60200ae3bde787dc6966645443fd588b361cecf1
plapicola <p.lapicola@gmail.com>
2019-05-07 14:56:17 -0600

    Updates foods routers

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
 var FoodController = require('../../../controllers/food_controller')
 
+/* GET all food items */
+router.get('/', FoodController.index);
 /* GET single food item */
-router.get('/:id', FoodController.show)
+router.get('/:id', FoodController.show);
 
 
 module.exports = router;
