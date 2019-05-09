051ad447dac57fa574b3d7a5ebced8c834975cc6
plapicola <p.lapicola@gmail.com>
2019-05-09 09:12:16 -0600

    Makes corrections to test and updates seeders to attempt to resolve issue where the associated meal or food records have not been written to the database before inserting mealfoods

--- seeders/20190507190805-foodAndMeals.js
+++ seeders/20190507190805-foodAndMeals.js
@@ -2,96 +2,97 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return Promise.all([
-      queryInterface.bulkInsert('Food', [
-        {
-          id: 1,
-          name: 'Banana',
-          calories: 105,
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          id: 2,
-          name: 'Egg',
-          calories: 75,
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          id: 3,
-          name: 'Salmon',
-          calories: 203,
-          createdAt: new Date(),
-          updatedAt: new Date()
-        }
-      ], {}),
-      queryInterface.bulkInsert('Meals', [
-        {
-          id: 1,
-          name: 'Breakfast',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          id: 2,
-          name: 'Lunch',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          id: 3,
-          name: 'Dinner',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        }
-      ], {}),
-      queryInterface.bulkInsert('MealFoods', [
-        {
-          FoodId: 1,
-          MealId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          FoodId: 2,
-          MealId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          FoodId: 2,
-          MealId: 2,
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          FoodId: 3,
-          MealId: 2,
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          FoodId: 1,
-          MealId: 3,
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          FoodId: 3,
-          MealId: 3,
-          createdAt: new Date(),
-          updatedAt: new Date()
-        }
-      ], {})
-    ])
+    return new Promise((resolve, reject) => {
+      Promise.all([
+        queryInterface.bulkInsert('Food', [
+          {
+            name: 'Banana',
+            calories: 105,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          {
+            name: 'Egg',
+            calories: 75,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          {
+            name: 'Salmon',
+            calories: 203,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          }
+        ], {}),
+        queryInterface.bulkInsert('Meals', [
+          {
+            id: 1,
+            name: 'Breakfast',
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          {
+            id: 2,
+            name: 'Lunch',
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          {
+            id: 3,
+            name: 'Dinner',
+            createdAt: new Date(),
+            updatedAt: new Date()
+          }
+        ], {})
+      ])
+      .then(() => {
+        resolve(queryInterface.bulkInsert('MealFoods', [
+          {
+            "FoodId": 1,
+            "MealId": 1,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          {
+            "FoodId": 2,
+            "MealId": 1,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          {
+            "FoodId": 2,
+            "MealId": 2,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          {
+            "FoodId": 3,
+            "MealId": 2,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          {
+            "FoodId": 1,
+            "MealId": 3,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          {
+            "FoodId": 3,
+            "MealId": 3,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          }
+        ], {}))
+      })
+    })
   },
 
   down: (queryInterface, Sequelize) => {
     return Promise.all([
+      queryInterface.bulkDelete('MealFoods', null, {}),
       queryInterface.bulkDelete('Food', null, {}),
-      queryInterface.bulkDelete('Meals', null, {}),
-      queryInterface.bulkDelete('MealFoods', null, {})
+      queryInterface.bulkDelete('Meals', null, {})
     ])
   }
 };
